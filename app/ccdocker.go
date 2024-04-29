8bd6a1c2149bea4512fc1dbc78bea0fd578957b0
iago sousa <iagosousavix@gmail.com>
2024-04-29 20:12:52 -0300

    Set namespace and hostname

--- app/ccdocker.go
+++ app/ccdocker.go
@@ -2,45 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"syscall"
 )
 
 func main() {
 
 	// Parse args
-	if len(os.Args) != 3 || os.Args[1] != "run" {
-		log.Fatalf("\nInvalid command: %s\n\nUse: \t ccdocker run \"<command>\"\n\n", strings.Join(os.Args, " "))
+	if len(os.Args) < 3 || os.Args[1] != "run" {
+		log.Fatalf("\nInvalid command: %s\n\nUse: \t ccdocker run \"<container>\" \"<command>\"\n\n", strings.Join(os.Args, " "))
 	}
 
-	runCommand(os.Args[2])
-
+	fmt.Println("Origin PID:: ", os.Getpid())
+	runCommand(os.Args[2], os.Args[3])
 }
 
-type TransformWriter struct {
-	w io.Writer
-}
-
-func (tw *TransformWriter) Write(p []byte) (n int, err error) {
-	transformedOutput := string(p)
-
-	defer tw.w.Write([]byte("\n>> "))
-	return tw.w.Write([]byte(transformedOutput))
-}
-
-func runCommand(command string) {
-	cmdArgs := strings.Split(command, " ")
+func runCommand(containerName string, command string) {
 	fmt.Println("Running command: ", command)
-	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
-	cmd.Stdout = &TransformWriter{os.Stdout}
+
+	cmd := exec.Command("/bin/sh", "-c", "hostname "+containerName+";"+command)
+
+	// Set process in new UTS namespace so hostname can be changed
+	// without impacting outside environment
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
-	err := cmd.Run()
-	if err != nil {
+
+	if err := cmd.Start(); err != nil {
 		log.Fatalf("Error running command: %s", err)
 	}
-}
+	fmt.Println("Child PID: ", cmd.Process.Pid)
 
+	//Set signal handlers to forward signals to child process
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatalf("Error waiting for command: %s", err)
+	}
+}
